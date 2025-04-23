1da835a2aa5f114039ddbc79bc8c81146b89b7bc
tamilvanan <tamilvanansambasivam@gmail.com>
2025-04-23 17:41:34 +0530

    simple port scanner

---
+++ port scanner/port-scanner.py
@@ -0,0 +1,13 @@
+import socket
+
+target = "scanme.nmap.org"  # or IP like "192.168.1.1"
+
+for port in range(1, 1025):  # Scan ports 1 through 1024
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.5)  # Timeout if no response
+    result = s.connect_ex((target, port))  # Try to connect
+
+    if result == 0:
+        print(f"[+] Port {port} is open")
+
+    s.close()
