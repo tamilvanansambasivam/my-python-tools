826ec7411c56f20f31e0782f266ada1a5bad7656
tamilvanan <tamilvanansambasivam@gmail.com>
2025-04-23 17:51:13 +0530

    refactored: replaced hardcoded target with user input

--- port scanner/port-scanner.py
+++ port scanner/port-scanner.py
@@ -1,6 +1,7 @@
 import socket
 
-target = "scanme.nmap.org"  # or IP like "192.168.1.1"
+target = input("enter target: ") 
+# enter target like "scanme.nmap.org" or IP like "192.168.1.1"
 
 for port in range(1, 1025):  # Scan ports 1 through 1024
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
